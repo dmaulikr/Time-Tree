a89dd87bbc810b89c3547d3ce1379c0432587034
Joseph Tsai <victoryandjoseph@gmail.com>
2015-07-04 17:57:42 +0800

    update

--- TimeTree/Login/LoginViewController.m
+++ TimeTree/Login/LoginViewController.m
@@ -13,11 +13,18 @@
 #import "Comms.h"
 #import "testViewController.h"
 #import <FacebookSDK/FacebookSDK.h>
+#import "ParseFacebookUtils/PFFacebookUtils.h"
 
 
 
-@interface LoginViewController ()<CommsDelegate>
-@property (weak, nonatomic) IBOutlet FBSDKLoginButton *FBLogin_Btn;
+
+@interface LoginViewController ()<CommsDelegate,FBLoginViewDelegate>
+{
+    NSUserDefaults *defaults;
+}
+
+//@property (weak, nonatomic) IBOutlet FBSDKLoginButton *FBLogin_Btn;
+@property (weak, nonatomic) IBOutlet FBLoginView *fbLoginView1;
 
 @end
 
@@ -27,37 +34,16 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    // set up NSUserDefaults
+    defaults=  [NSUserDefaults standardUserDefaults];
 
-//    if ([FBSDKAccessToken currentAccessToken]) {
-//        NSLog(@"login succcessful!");
-//    }
-//    self.aa.readPermissions= @[@"public_profile", @"email", @"user_friends"];
-//
-//    FBSDKLoginManager *login = [[FBSDKLoginManager alloc] init];
-//
-//    [login logInWithReadPermissions:@[@"email"] handler:^(FBSDKLoginManagerLoginResult *result, NSError *error) {
-//        if (error) {
-//            // Process error
-//        } else if (result.isCancelled) {
-//            // Handle cancellations
-//        } else {
-//            // If you ask for multiple permissions at once, you
-//            // should check if specific permissions missing
-//            if ([result.grantedPermissions containsObject:@"email"]) {
-//                // Do work
-//            }
-//        }
-//    }];
+    // sdk3.5 old version (old method)
+    self.fbLoginView1.publishPermissions=@[@"public_profile", @"email", @"user_friends"];
     
-    FBLoginView *loginView =[[FBLoginView alloc] initWithReadPermissions:@[@"public_profile", @"email", @"user_friends"]];
+    self.fbLoginView1.delegate=self;
     
-    
-//    [self.view addSubview:loginView];
-    [PFUser logOut];
-
-    self.FBLogin_Btn.delegate=self;
+ 
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -66,38 +52,109 @@
 
 
 
-- (IBAction)FBlogin:(id)sender {
+//- (IBAction)FBlogin:(id)sender {
+//    
+//    // Disable the Login button to prevent multiple touches
+//    [self.FBLogin_Btn setEnabled:NO];
+//    
+//    // Do the login
+//    [Comms login:self];
+//
+//}
+
+//- (void) commsDidLogin:(BOOL)loggedIn {
+//    // Re-enable the Login button
+//    [self.FBLogin_Btn setEnabled:YES];
+//    
+//    
+//    // Did we login successfully ?
+//    if (loggedIn) {
+////        
+////        UIStoryboard *sb=[UIStoryboard storyboardWithName:@"Login" bundle:nil];
+////        testViewController *vc= [sb instantiateViewControllerWithIdentifier:@"LoginSuccessful"];
+////        
+////        [self.navigationController pushViewController:vc animated:YES];
+////      
+////        
+//        
+////        if ([FBSDKProfile currentProfile]) {
+////            [FBSDKProfile enableUpdatesOnAccessTokenChange:YES];
+////            NSLog(@"user is -- %@",[[FBSDKProfile currentProfile]name]);
+////        }
+//        
+//        
+//    } else {
+//        // Show error alert
+//        [[[UIAlertView alloc] initWithTitle:@"Login Failed"
+//                                    message:@"Facebook Login failed. Please try again"
+//                                   delegate:nil
+//                          cancelButtonTitle:@"Ok"
+//                          otherButtonTitles:nil] show];
+//    }
+//}
+
+#pragma mark -  FBLoginView delegate
+
+// logged
+- (void)loginViewShowingLoggedInUser:(FBLoginView *)loginView;
+{
     
-    // Disable the Login button to prevent multiple touches
-    [self.FBLogin_Btn setEnabled:NO];
-    
-    // Do the login
-    [Comms login:self];
+    UIStoryboard *sb=[UIStoryboard storyboardWithName:@"Login" bundle:nil];
+    testViewController *vc= [sb instantiateViewControllerWithIdentifier:@"LoginSuccessful"];
+    [self.navigationController pushViewController:vc animated:YES];
+
+
+}
+
+// parse login via FB (PFUser)
+-(void)parseLogin{
+    [PFFacebookUtils logInWithPermissions:@[@"public_profile", @"email", @"user_friends"] block:^(PFUser *user, NSError *error) {
+        // Was login successful ?
+        if (!user) {
+            if (!error) {
+                NSLog(@"The user cancelled the Facebook login.");
+            } else {
+                NSLog(@"An error occurred: %@", error.localizedDescription);
+            }
+            
+        } else {
+            // Callback - login successful
+            PFUser *pfUser=[PFUser currentUser];
+            NSLog(@"id is %@",pfUser.objectId);
+            pfUser.email=[defaults objectForKey:@"email"];
+            [pfUser saveInBackground];
+        }
+    }];
     
 }
 
-- (void) commsDidLogin:(BOOL)loggedIn {
-    // Re-enable the Login button
-    [self.FBLogin_Btn setEnabled:YES];
+- (void)loginViewFetchedUserInfo:(FBLoginView *)loginView
+                            user:(id<FBGraphUser>)user {
+    //將 layout 是 profilePictureView 將會顯示使用者的大頭照
+    //    self.profilePictureView.profileID = user.id;
+    //nameLabel 的內容將會改變成使用者的名稱
+    NSLog(@"user is %@",user);
     
+    [defaults setObject: [user objectForKey:@"email"] forKey:@"email"];
+    [defaults synchronize];
     
-    // Did we login successfully ?
-    if (loggedIn) {
-        
-        UIStoryboard *sb=[UIStoryboard storyboardWithName:@"Login" bundle:nil];
-        testViewController *vc= [sb instantiateViewControllerWithIdentifier:@"LoginSuccessful"];
-        
-        [self.navigationController pushViewController:vc animated:YES];
-      
-        
-    } else {
-        // Show error alert
-        [[[UIAlertView alloc] initWithTitle:@"Login Failed"
-                                    message:@"Facebook Login failed. Please try again"
-                                   delegate:nil
-                          cancelButtonTitle:@"Ok"
-                          otherButtonTitles:nil] show];
-    }
+    [self parseLogin];
+   
+}
+
+
+//log out
+- (void)loginViewShowingLoggedOutUser:(FBLoginView *)loginView;
+{
+//    [defaults setObject:nil forKey:@"email"];
+//    [defaults synchronize];
+//    [PFUser logOut];
+}
+
+- (void)loginView:(FBLoginView *)loginView handleError:(NSError *)error{
+    
+    NSLog(@"login in error %@",error);
+
 }
 
 @end
--- TimeTree/TimeTreeVC/testViewController.m
+++ TimeTree/TimeTreeVC/testViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "testViewController.h"
+#import "Parse/Parse.h"
+
 
 @interface testViewController ()
+{
+}
 
 @end
 
@@ -17,21 +21,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+-(void)viewDidAppear:(BOOL)animated{
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
 }
-*/
-
 @end
