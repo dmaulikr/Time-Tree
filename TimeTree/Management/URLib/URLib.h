c994a6c7e0f9f5d403636d94107ed5a1309b08e7
Joseph Tsai <victoryandjoseph@gmail.com>
2015-07-10 22:30:30 +0800

    add

---
+++ TimeTree/Management/URLib/URLib.h
@@ -0,0 +1,17 @@
+//
+//  URLib.h
+//  TimeTree
+//
+//  Created by Joseph on 2015/7/10.
+//  Copyright (c) 2015年 dosomethingq. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface URLib : NSObject
+
+#define FB_GRAPH @"https://graph.facebook.com/"
+#define FB_PROFILE_IMG @"/picture?width=100&height=100"
+
+
+@end
---
+++ TimeTree/Management/URLib/URLib.m
@@ -0,0 +1,13 @@
+//
+//  URLib.m
+//  TimeTree
+//
+//  Created by Joseph on 2015/7/10.
+//  Copyright (c) 2015年 dosomethingq. All rights reserved.
+//
+
+#import "URLib.h"
+
+@implementation URLib
+
+@end
